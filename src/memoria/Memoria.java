431afc225423d04df7dea5a71c9e8465e55e3324
gaso <gustavo2840@gmail.com>
2015-11-19 19:27:23 -0500

    completar el javadoc

--- src/memoria/Memoria.java
+++ src/memoria/Memoria.java
@@ -2,10 +2,15 @@ package memoria;
 
 import gui.PrincipalUI;
 
+/**
+ * Lanzador de Aplicacion
+ *
+ * @author Cristian Toro, Gustavo Salgado y Laura Rúa
+ * @version 1.0
+ */
 public class Memoria {
 
     public static void main(String[] args) {
-
         PrincipalUI pri = new PrincipalUI();
         pri.setVisible(true);
 
